ca6140f8b2093568c9e31fdaf3dbe6d1a8f8c858
Bahadir SAYLAN <bahadirsaylan@gmail.com>
2020-01-22 16:13:59 +0300

    removed project files

--- api/src/main/java/io/cloudhpc/api/repository/ClusterRepository.java
+++
@@ -1,7 +0,0 @@
-package io.cloudhpc.api.repository;
-
-import io.cloudhpc.api.entity.Cluster;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClusterRepository extends JpaRepository<Cluster,Integer> {
-}
